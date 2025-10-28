5612ef8317d59c6567ed1bbaded841a904557751
David Brunger <david.brunger@newday.co.uk>
2025-10-28 13:50:42 +0000

    Update README.md

--- GenerationTest.Cmd/Tester.cs
+++ GenerationTest.Cmd/Tester.cs
@@ -1,5 +1,6 @@
 ﻿using WS.DomainModelling.DiscriminatedUnion;
 using WS.DomainModelling.BasicWrapper;
+using WS.DomainModelling.Common;
 
 namespace GenerationTest.Cmd;
 
@@ -10,6 +11,7 @@ namespace GenerationTest.Cmd;
 [Option("JammyWhammy", OfType = typeof((int AnInt, string)))]
 public partial class Tester<Q>
 {
+    Option<Score> score { get; } = NaturalInteger.Create(5).Bind(home => NaturalInteger.Create(2).Map(away => new Score(home, away)));
 }
 
 [BasicWrapper(typeof(string), nameof(Validate))]
@@ -19,4 +21,39 @@ public partial class String50
     {
         return source != null && source.Length <= 50;
     }
+}
+
+[BasicWrapper(typeof(int), nameof(Validate))]
+public partial class NaturalInteger
+{
+    private static bool Validate(int value) => value >= 0;
+}
+
+public record Score(NaturalInteger Home, NaturalInteger Away);
+
+[Option("Success", OfGeneric = "Q")]
+[Option("Failed")]
+[Option("FileNotFoundError", OfType = typeof(string))]
+[Option("IndexOutOfBoundsError", OfType = typeof((int AnInt, string)))]
+public partial class DetailedResult<Q>
+{
+}
+
+DetailedResult<List<string>> DoSomething()
+{
+    var index = 9;
+    if (DateTime.Now > DateTime.Now)
+    {
+        return DetailedResult<List<string>>.IndexOutOfBoundsError((index, "Message"));
+    }
+    if (DateTime.Now > DateTime.Now)
+    {
+        return DetailedResult<List<string>>.FileNotFoundError("Message");
+    }
+    var messages = new[] { "Hello" }.ToList();
+    if (messages.Count > 0)
+    {
+        return DetailedResult<List<string>>.Failed;
+    }
+    return DetailedResult<List<string>>.Success(messages);
 }
--- README.md
+++ README.md
@@ -3,3 +3,17 @@ Test out dotnet code generation to create a genearator for F# style discriminate
 
 ## Inspiration
 - [*Domain Modeling Made Functional*](https://pragprog.com/titles/swdddf/domain-modeling-made-functional/) - [Scott Wlaschin](https://scottwlaschin.com/)
+
+## Usage
+
+### BasicWrapper
+
+Basic Wrapper generates a class that wraps a single type with validation logic.
+
+#### Definition
+
+```csharp
+[BasicWrapper(typeof(int), nameof(Validate))]
+public partial class PositiveInterger
+
+```
