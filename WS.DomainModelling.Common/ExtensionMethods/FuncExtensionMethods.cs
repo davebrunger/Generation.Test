ec9ce5b1e2f505ab8b0cb545aba6b29fb702d5f6
David Brunger <david.brunger@newday.co.uk>
2025-12-02 17:49:33 +0000

    Added Tee, TryCatch, DoubleMap and Plus functions to Result type

--- WS.DomainModelling.Common/ExtensionMethods/FuncExtensionMethods.cs
+++ WS.DomainModelling.Common/ExtensionMethods/FuncExtensionMethods.cs
@@ -2,8 +2,26 @@
 
 public static class Func
 {
+    // Functional style methods for Func
+
     public static Func<T, V> Compose<T, U, V>(this Func<T, U> func1, Func<U, V> func2)
     {
         return t => func2(func1(t));
+    }
+
+    public static Func<T, T> Tee<T>(this Action<T> action)
+    {
+        return t =>
+        {
+            action(t);
+            return t;
+        };
+    }
+
+    // Idomatic C# style methods for Func
+
+    public static T Tee<T>(this T input, Action<T> action)
+    {
+        return Tee(action)(input);
     }
 }
--- WS.DomainModelling.Common/Result.cs
+++ WS.DomainModelling.Common/Result.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace WS.DomainModelling.Common;
+﻿namespace WS.DomainModelling.Common;
 
 public abstract class Result<T, TError>
 {
@@ -54,11 +52,11 @@ public abstract class Result<T, TError>
     }
 
 
-    public static Result<T, TError> Success(T Success) => new SuccessResult(Success);
+    public static Result<T, TError> Success(T success) => new SuccessResult(success);
 
-    public static Result<T, TError> Error(TError Error) => new ErrorResult(Error);
+    public static Result<T, TError> Error(TError error) => new ErrorResult(error);
 
-    public Option<T> AsSuccess() => Match((Success) => Option<T>.Some(Success), (_) => Option<T>.None);
+    public Option<T> AsSuccess() => Match(Option<T>.Some, (_) => Option<T>.None);
 
     public Option<TError> AsError() => Match((_) => Option<TError>.None, (Error) => Option<TError>.Some(Error));
 
@@ -82,15 +80,18 @@ public static class Result
 
     public static Func<Result<T, TError>, Result<U, TError>> Bind<T, U, TError>(Func<T, Result<U, TError>> function)
     {
-        return input => input.Match(t => function(t), e => Result<U, TError>.Error(e));
+        return input => input.Match(
+            t => function(t), 
+            e => Result<U, TError>.Error(e)
+        );
     }
 
-    public static Func<Result<(T Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>, Result<(U Value, IReadOnlyList<TMessage>Messages), IReadOnlyList<TError>>> Bind<T, U, TMessage, TError>(
+    public static Func<Result<(T Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>, Result<(U Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>> Bind<T, U, TMessage, TError>(
         Func<T, Result<(U Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>> function)
     {
         return input => input.Match(
             t => function(t.Value).Match(
-                u => (u.Value, t.Messages.Concat(u.Messages).ToImmutableList()),
+                u => (u.Value, [.. t.Messages, .. u.Messages]),
                 Result<(U Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>.Error),
             Result<(U Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>.Error);
     }
@@ -98,6 +99,60 @@ public static class Result
     public static Func<Result<T, TError>, Result<U, TError>> Map<T, U, TError>(Func<T, U> function)
     {
         return Bind(function.Compose(Result<U, TError>.Success));
+    }
+
+    public static Func<T, Result<U, TError>> TryCatch<T, U, TError>(Func<T, U> function, Func<Exception, TError> error)
+    {
+        return input =>
+        {
+            try
+            {
+                return function(input);
+            }
+            catch (Exception ex)
+            {
+                return error(ex);
+            }
+        };
+    }
+
+    public static Func<Result<T, TError>, Result<U, TError>> DoubleMap<T, U, TError>(Func<T, U> success, Func<TError, TError> error)
+    {
+        return input => input.Match<Result<U, TError>>(
+            t => success(t),
+            e => error(e)
+        );
+    }
+
+    public static Func<T, Result<W, TError>> Plus<T, U, V, W, TError>(
+        Func<U, V, W> combineSuccess,
+        Func<TError, TError, TError> combineError,
+        Func<T, Result<U, TError>> function1,
+        Func<T, Result<V, TError>> function2)
+    {
+        return input => function1(input).Match(
+            success1 => function2(input).Match<Result<W, TError>>(
+                success2 => combineSuccess(success1, success2),
+                error2 => error2
+            ),
+            error1 => function2(input).Match<Result<W, TError>>(
+                _ => error1,
+                error2 => combineError(error1, error2)
+            )
+        );
+    }
+
+    public static Func<T, Result<(W Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>> Plus<T, U, V, W, TMessage, TError>(
+        Func<U, V, W> combineSuccess,
+        Func<T, Result<(U Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>> function1,
+        Func<T, Result<(V Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>> function2)
+    {
+        return Plus<T, (U Value, IReadOnlyList<TMessage> Messages), (V Value, IReadOnlyList<TMessage> Messages), (W Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>(
+            (success1, success2) => (combineSuccess(success1.Value, success2.Value), [.. success1.Messages, .. success2.Messages]),
+            (error1, error2) => [.. error1, .. error2],
+            function1,
+            function2
+        );
     }
 
     // Idomatic C# style methods for Result
@@ -115,5 +170,34 @@ public static class Result
     public static Result<U, TError> Map<T, U, TError>(this Result<T, TError> input, Func<T, U> function)
     {
         return Map<T, U, TError>(function)(input);
+    }
+
+    public static Result<U, TError> TryCatch<T, U, TError>(this T input, Func<T, U> function, Func<Exception, TError> convert)
+    {
+        return TryCatch(function, convert)(input);
+    }
+
+    public static Result<U, TError> DoubleMap<T, U, TError>(this Result<T, TError> input, Func<T, U> success, Func<TError, TError> error)
+    {
+        return DoubleMap(success, error)(input);
+    }
+
+    public static Result<W, TError> Plus<T, U, V, W, TError>(
+        this T input,
+        Func<U, V, W> combineSuccess,
+        Func<TError, TError, TError> combineError,
+        Func<T, Result<U, TError>> function1,
+        Func<T, Result<V, TError>> function2)
+    {
+        return Plus(combineSuccess, combineError, function1, function2)(input);
+    }
+
+    public static Result<(W Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>> Plus<T, U, V, W, TMessage, TError>(
+        this T input,
+        Func<U, V, W> combineSuccess,
+        Func<T, Result<(U Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>> function1,
+        Func<T, Result<(V Value, IReadOnlyList<TMessage> Messages), IReadOnlyList<TError>>> function2)
+    {
+        return Plus(combineSuccess, function1, function2)(input);
     }
 }
